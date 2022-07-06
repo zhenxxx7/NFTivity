fcbd98325e70c47302b894867fd5233d2aaf7b8e
zhenxxx7 <jordy.zeventh@gmail.com>
2022-07-07 01:15:15 +0700

    add navbar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,38 +4,38 @@ import { Footer } from "flowbite-react";
 export default function App() {
     return (
         <div>
-        <Footer container={true}>
-  <div className="w-full text-center"  class="bg-white dark:bg-slate-800">
-    <div className="w-full justify-between sm:flex sm:items-center sm:justify-between ">
-      <Footer.Brand
-        href="https://flowbite.com"
-        src="https://flowbite.com/docs/images/logo.svg"
-        alt="Flowbite Logo"
-        name="Flowbite"
-      />
-      <Footer.LinkGroup>
-        <Footer.Link href="#">
-          About
-        </Footer.Link>
-        <Footer.Link href="#">
-          Privacy Policy
-        </Footer.Link>
-        <Footer.Link href="#">
-          Licensing
-        </Footer.Link>
-        <Footer.Link href="#">
-          Contact
-        </Footer.Link>
-      </Footer.LinkGroup>
-    </div>
-    <Footer.Divider />
-    <Footer.Copyright
-      href="#"
-      by="Flowbite™"
-      year={2022}
-    />
-  </div>
-</Footer>
+          <Footer container={true}>
+            <div className="w-full text-center"  class="bg-white dark:bg-slate-800">
+              <div className="w-full justify-between sm:flex sm:items-center sm:justify-between ">
+                <Footer.Brand
+                  href="https://flowbite.com"
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  alt="Flowbite Logo"
+                  name="Flowbite"
+                />
+                <Footer.LinkGroup>
+                  <Footer.Link href="#">
+                    About
+                  </Footer.Link>
+                  <Footer.Link href="#">
+                    Privacy Policy
+                  </Footer.Link>
+                  <Footer.Link href="#">
+                    Licensing
+                  </Footer.Link>
+                  <Footer.Link href="#">
+                    Contact
+                  </Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+              <Footer.Divider />
+              <Footer.Copyright
+                href="#"
+                by="Flowbite™"
+                year={2022}
+              />
+            </div>
+          </Footer>
         </div>
     );
 }
